ab2b71e3ce68ea621c2fec0d8046563d467525bf
shige <shige@asiainfo.com>
2022-10-17 20:37:44 +0800

    feat: add menu storybook

---
+++ src/components/Menu/menu.stories.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import Menu from "./menu";
+import SubMenu from "./subMenu";
+import MenuItem from "./menuItem";
+
+const menuMeta: ComponentMeta<typeof Menu> = {
+  component: Menu,
+  subcomponents: { 'SubMenu': SubMenu, 'MenuItem': MenuItem },
+  title: "Menu 菜单",
+  args: {
+    // 这里也可以使用args，但是优先级比单个的低
+    defaultIndex: '1'
+  },
+  // argTypes: {
+  //   defaultIndex: {
+  //     control: 'color',
+  //     description: 'normal test'
+  //   }
+  // }
+}
+export default menuMeta
+
+const Template: ComponentStory<typeof Menu> = (args) => (
+  <Menu {...args} >
+    <MenuItem>
+      cool link
+    </MenuItem>
+    <MenuItem>
+      cool link 2
+    </MenuItem>
+    <MenuItem disabled>
+      disabled
+    </MenuItem> 
+    <SubMenu title="下拉选项">
+      <MenuItem>
+        下拉选项一
+      </MenuItem>
+      <MenuItem>
+        下拉选项二
+      </MenuItem>    
+    </SubMenu>
+  </Menu>
+)
+
+export const ADefaultMenu = Template.bind({})
+ADefaultMenu.args = {}
+ADefaultMenu.storyName = '默认Menu'
+
+export const BClickMenu = Template.bind({})
+BClickMenu.args = {
+  mode: 'vertical'
+}
+BClickMenu.storyName = '纵向的 Menu'
+
+export const COpenedMenu = Template.bind({})
+COpenedMenu.args = {
+  mode: 'vertical',
+  defaultOpenSubMenus: ['3']
+}
+COpenedMenu.argTypes = {
+  // 可以控制具体的控制器类型，例如下方的color取色器
+  // defaultIndex: {
+  //   control: 'color'
+  // }
+}
+COpenedMenu.storyName = '默认展开的纵向 Menu'
